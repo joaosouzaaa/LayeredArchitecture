76eef4d792f37d50d02b41757acc5302976e7320
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-25 11:47:19 -0300

    Tests: ConnectionStringFactory unit tests.

---
+++ UnitTests/FactoriesTests/ConnectionStringFactoryTests.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.Configuration;
+using FlowerShop.Business.Factories;
+
+namespace UnitTests.FactoriesTests;
+public sealed class ConnectionStringFactoryTests
+{
+    [Fact]
+    public void GetConnectionString_ReturnsContainerConnection()
+    {
+        // A
+        const string containerConnection = "test";
+        var inMemoryCollection = new Dictionary<string, string>()
+        {
+            {"ConnectionStrings:ContainerConnection", containerConnection }
+        };
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemoryCollection!)
+            .Build();
+
+        Environment.SetEnvironmentVariable("DOCKER_ENVIROMENT", "DOCKER_ENV");
+
+        // A
+        var connectionStringResult = configuration.GetConnectionString();
+
+        // A
+        Assert.Equal(containerConnection, connectionStringResult);
+    }
+
+    [Fact]
+    public void GetConnectionString_ReturnsLocalConnection()
+    {
+        // A
+        const string localConnection = "joao";
+        var inMemoryCollection = new Dictionary<string, string>()
+        {
+            {"ConnectionStrings:LocalConnection", localConnection}
+        };
+        IConfiguration configuration = new ConfigurationBuilder()
+            .AddInMemoryCollection(inMemoryCollection!)
+            .Build();
+
+        Environment.SetEnvironmentVariable("DOCKER_ENVIROMENT", "any value");
+
+        // A
+        var connectionStringResult = configuration.GetConnectionString();
+
+        // A
+        Assert.Equal(localConnection, connectionStringResult);
+    }
+}
