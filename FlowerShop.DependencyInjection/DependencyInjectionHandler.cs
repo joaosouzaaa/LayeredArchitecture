7b5e9205870d61cdad1f29926f65314077ec6823
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-25 12:01:28 -0300

    Feat: Adding Validators DependencyInjection.

--- FlowerShop.DependencyInjection/DependencyInjectionHandler.cs
+++ FlowerShop.DependencyInjection/DependencyInjectionHandler.cs
@@ -18,5 +18,6 @@ public static class DependencyInjectionHandler
 
         services.AddSettingsDependencyInjection();
         services.AddRepositoriesDependencyInjection();
+        services.AddValidatorsDependencyInjection();
     }
 }
---
+++ FlowerShop.DependencyInjection/ValidatorsDependencyInjection.cs
@@ -0,0 +1,14 @@
+﻿using FlowerShop.Business.Settings.ValidatorSettings;
+using FlowerShop.Domain.Entites;
+using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowerShop.DependencyInjection;
+internal static class ValidatorsDependencyInjection
+{
+    internal static void AddValidatorsDependencyInjection(this IServiceCollection services)
+    {
+        services.AddScoped<IValidator<Flower>, FlowerValidator>();
+        services.AddScoped<IValidator<Shop>, ShopValidator>();
+    }
+}
