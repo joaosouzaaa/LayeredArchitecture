4b15f0e4298bed5dff55379ebe427171bee2f448
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-25 00:08:13 -0300

    Feat: Adding initial migration and migration handler.

---
+++ FlowerShop.DependencyInjection/MigrationHandler.cs
@@ -0,0 +1,23 @@
+﻿using FlowerShop.Infrastructure.DatabaseContexts;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FlowerShop.DependencyInjection;
+public static class MigrationHandler
+{
+    public static void MigrateDatabase(this IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        using var appDbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
+        try
+        {
+            appDbContext.Database.Migrate();
+        }
+        catch
+        {
+            throw;
+        }
+    }
+}
