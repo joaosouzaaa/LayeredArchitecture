4b78b4f220e8ae6880748babc501e32f3bfc77f1
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-25 00:16:08 -0300

    Feat: Adding ShopRepository.

---
+++ FlowerShop.Business/Interfaces/Repositories/IShopRepository.cs
@@ -0,0 +1,12 @@
+﻿using FlowerShop.Domain.Entites;
+
+namespace FlowerShop.Business.Interfaces.Repositories;
+public interface IShopRepository
+{
+    Task<bool> AddAsync(Shop shop);
+    Task<bool> UpdateAsync(Shop shop);
+    Task<bool> ExistsAsync(int id);
+    Task<bool> DeleteAsync(int id);
+    Task<Shop?> GetByIdAsync(int id);
+    Task<List<Shop>> GetAllAsync();
+}
---
+++ FlowerShop.Infrastructure/Repositories/ShopRepository.cs
@@ -0,0 +1,45 @@
+﻿using FlowerShop.Business.Interfaces.Repositories;
+using FlowerShop.Domain.Entites;
+using FlowerShop.Infrastructure.DatabaseContexts;
+using FlowerShop.Infrastructure.Repositories.BaseRepositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowerShop.Infrastructure.Repositories;
+public sealed class ShopRepository : BaseRepository<Shop>, IShopRepository
+{
+    public ShopRepository(AppDbContext dbContext) : base(dbContext)
+    {
+    }
+
+    public async Task<bool> AddAsync(Shop shop)
+    {
+        await DbContextSet.AddAsync(shop);
+
+        return await SaveChangesAsync();
+    }
+
+    public Task<bool> UpdateAsync(Shop shop)
+    {
+        _dbContext.Entry(shop).State = EntityState.Modified;
+
+        return SaveChangesAsync();
+    }
+
+    public Task<bool> ExistsAsync(int id) =>
+        DbContextSet.AsNoTracking().AnyAsync(s => s.Id == id);
+
+    public async Task<bool> DeleteAsync(int id)
+    {
+        var shop = await DbContextSet.FirstOrDefaultAsync(s => s.Id == id);
+
+        DbContextSet.Remove(shop!);
+
+        return await SaveChangesAsync();
+    }
+
+    public Task<Shop?> GetByIdAsync(int id) =>
+        DbContextSet.AsNoTracking().FirstOrDefaultAsync(s => s.Id == id);
+
+    public Task<List<Shop>> GetAllAsync() =>
+        DbContextSet.AsNoTracking().ToListAsync();
+}
