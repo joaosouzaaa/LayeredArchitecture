747109d9108d780a0919384066e3bbc45cac1ab0
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-25 11:59:25 -0300

    Feat: Adding Flower and Shop Validators.

---
+++ FlowerShop.Business/Settings/ValidatorSettings/FlowerValidator.cs
@@ -0,0 +1,20 @@
+﻿using FlowerShop.Business.Extensions;
+using FlowerShop.Domain.Entites;
+using FlowerShop.Domain.Enums;
+using FluentValidation;
+
+namespace FlowerShop.Business.Settings.ValidatorSettings;
+public sealed class FlowerValidator : AbstractValidator<Flower>
+{
+    public FlowerValidator()
+    {
+        RuleFor(f => f.Name).NotEmpty().Length(3, 100)
+            .WithMessage(EMessage.InvalidLength.Description().FormatTo("Name", "3 to 100"));
+
+        RuleFor(f => f.Color).NotEmpty().Length(3, 100)
+            .WithMessage(EMessage.InvalidLength.Description().FormatTo("Color", "3 to 100"));
+        
+        RuleFor(f => f.Species).NotEmpty().Length(3, 100)
+            .WithMessage(EMessage.InvalidLength.Description().FormatTo("Species", "3 to 100"));
+    }
+}
