ac5529d85812b1fb27dcf151d81c232bf820c2c4
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-25 00:11:17 -0300

    Feat: Adding BaseRepository.

---
+++ FlowerShop.Infrastructure/Repositories/BaseRepositories/BaseRepository.cs
@@ -0,0 +1,24 @@
+﻿using FlowerShop.Infrastructure.DatabaseContexts;
+using Microsoft.EntityFrameworkCore;
+
+namespace FlowerShop.Infrastructure.Repositories.BaseRepositories;
+public abstract class BaseRepository<TEntity> : IDisposable
+    where TEntity : class
+{
+    protected readonly AppDbContext _dbContext;
+    protected DbSet<TEntity> DbContextSet => _dbContext.Set<TEntity>();
+
+    protected BaseRepository(AppDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public void Dispose()
+    {
+        _dbContext.Dispose();
+        GC.SuppressFinalize(this);
+    }
+
+    protected async Task<bool> SaveChangesAsync() =>
+        await _dbContext.SaveChangesAsync() > 0;
+}
