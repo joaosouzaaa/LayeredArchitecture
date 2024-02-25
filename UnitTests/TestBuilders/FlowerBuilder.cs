703f4b0f2e5c295c6bb7c6127bf03c21403a19ad
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-25 12:09:57 -0300

    Tests: Adding TestBuilders.

---
+++ UnitTests/TestBuilders/FlowerBuilder.cs
@@ -0,0 +1,48 @@
+﻿using FlowerShop.Domain.Entites;
+using FlowerShop.Domain.Enums;
+
+namespace UnitTests.TestBuilders;
+public sealed class FlowerBuilder
+{
+    private readonly int _id = 123;
+    private string _name = "test";
+    private string _color = "test";
+    private string _species = "flower";
+    private readonly EBloomingSeason _bloomingSeason = EBloomingSeason.Winter;
+    private readonly int _shopId = 123;
+
+    public static FlowerBuilder NewObject() =>
+        new();
+
+    public Flower DomainBuild() =>
+        new()
+        {
+            Id = _id,
+            Name = _name,
+            Color = _color,
+            Species = _species,
+            BloomingSeason = _bloomingSeason,
+            ShopId = _shopId
+        };
+
+    public FlowerBuilder WithName(string name)
+    {
+        _name = name;
+
+        return this;
+    }
+
+    public FlowerBuilder WithColor(string color)
+    {
+        _color = color;
+
+        return this;
+    }
+
+    public FlowerBuilder WithSpecies(string species)
+    {
+        _species = species;
+
+        return this;
+    }
+}
---
+++ UnitTests/TestBuilders/ShopBuilder.cs
@@ -0,0 +1,47 @@
+﻿using FlowerShop.Domain.Entites;
+
+namespace UnitTests.TestBuilders;
+public sealed class ShopBuilder
+{
+    private readonly int _id = 10;
+    private string _name = "test";
+    private string _location = "location";
+    private string _email = "test@email.com";
+    private readonly DateTime _creationDate = DateTime.Now;
+    private List<Flower> _flowerList = [];
+
+    public static ShopBuilder NewObject() =>
+        new();
+
+    public Shop DomainBuild() =>
+        new()
+        {
+            Id = _id,
+            Name = _name,
+            Location = _location,
+            Email = _email,
+            CreationDate = _creationDate,
+            Flowers = _flowerList
+        };
+
+    public ShopBuilder WithName(string name)
+    {
+        _name = name;
+
+        return this;
+    }
+
+    public ShopBuilder WithLocation(string location)
+    {
+        _location = location;
+
+        return this;
+    }
+
+    public ShopBuilder WithEmail(string email)
+    {
+        _email = email;
+
+        return this;
+    }
+}
