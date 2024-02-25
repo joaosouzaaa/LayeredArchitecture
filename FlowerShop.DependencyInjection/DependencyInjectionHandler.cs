96a9bb3195a9e253c0f8d2b6e8ae8fcafbf6960d
joaosouzaaa <joaoasouza982@gmail.com>
2024-02-24 23:50:49 -0300

    Feat: Adding ConnectionStringFactory and DbContext DependencyInjection.

--- FlowerShop.DependencyInjection/DependencyInjectionHandler.cs
+++ FlowerShop.DependencyInjection/DependencyInjectionHandler.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using FlowerShop.Infrastructure.DatabaseContexts;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.EntityFrameworkCore;
+using FlowerShop.Business.Factories;
 
 namespace FlowerShop.DependencyInjection;
 public static class DependencyInjectionHandler
 {
     public static void AddDependencyInjection(this IServiceCollection services, IConfiguration configuration)
     {
+        services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseSqlServer(configuration.GetConnectionString());
+            options.EnableDetailedErrors();
+            options.EnableSensitiveDataLogging();
+        });
+
         services.AddSettingsDependencyInjection();
     }
 }
